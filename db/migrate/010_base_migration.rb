bb018a63f90ee9d971f61dde8700809e13d3b45d
andyl <andy@r210.com>
2011-02-03 11:48:31 -0800

    added first migrations

--- db/migrate/010_base_migration.rb
+++ db/migrate/010_base_migration.rb
@@ -4,17 +4,64 @@ class BaseMigration < ActiveRecord::Migration
   # mpa (member personal availabliity) , doa (duty officer availability)
   # page_log
   def self.up
-    create_table :actions do |t|
-      t.string   :digest
-      t.string   :kind,     :default => "event"
-      t.string   :title
-      t.string   :location, :default => "TBA"
-      t.string   :leaders,  :default => "TBA"
-      t.date     :start,    :default => Time.now
-      t.date     :finish
-      t.text     :description
-      t.boolean  :first_in_year, :default => false
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
       t.timestamps
+    end
+    create_table :addresses do |t|
+      t.integer :user_id
+      t.string  :atype
+      t.string  :addr1
+      t.string  :addr2
+      t.string  :city
+      t.string  :state
+      t.string  :zip
+      t.timestamps
+    end
+    create_table :phones do |t|
+      t.integer :user_id
+      t.string  :ptype
+      t.string  :number
+      t.string  :pagable
+      t.timestamps
+    end
+    create_table :emails do |t|
+      t.integer :user_id
+      t.string  :etype
+      t.string  :address
+      t.timestamps
+    end
+    create_table :roles do |t|
+      t.timestamps
+    end
+    create_table :photos do |t|
+      t.integer    :user_id
+      t.string     :filename
+      t.timestamps
+    end
+    create_table :mpas do |t|
+      t.integer   :user_id
+      t.date      :start
+      t.date      :end
+      t.timestamps
+    end
+    create_table :doas do |t|
+      t.integer      :week
+      t.timestamps
+    end
+    create_table :do_assignments do |t|
+      t.integer       :user_id
+      t.timestamps
+    end
+    create_table :messages do |t|
+      t.integer       :user_id         # author
+      t.string        :contents
+      t.timestamps
+    end
+    create_table :distributions do |t|
+      t.integer      :user_id
+      t.integer      :message_id
     end
   end
 end
