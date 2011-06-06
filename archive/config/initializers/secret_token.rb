16771556e627cabdf76f67f356eb21136fd46250
andyl <andy@r210.com>
2011-06-05 18:18:56 -0700

    removed archive

--- archive/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Bnet::Application.config.secret_token = '4e9a85f6ccceab44214461155b2015a313ec46e4d95ef5b15df68b954e04f96e9008fbc33bdb8c027d0c807bab35cac2ae742658c0ea7d85403c1b3e54bf96fc'
