eccc86c8046999b9f987ef58de96a3d84d5547df
andyl <andy@r210.com>
2011-06-06 18:49:40 -0700

    adding phones works

--- app/assets/javascripts/members_edit_form.js.coffee
+++ app/assets/javascripts/members_edit_form.js.coffee
@@ -2,6 +2,15 @@
 This is code to support the members/edit form.
 ###
 
+window.cleanup = (string) ->
+  string.replace(/\&amp;/g,'&').replace(/\&lt;/g,'<').replace(/\&quot;/g,'"').replace(/\&gt;/g,'>')
+
+window.add_fields = (link, association, content) ->
+  new_id = new Date().getTime();
+  regexp = new RegExp("new_" + association, "g")
+  str = content.replace(regexp, new_id)
+  $(link).parent().next().after(cleanup(content.replace(regexp, new_id)))
+
 $(document).ready ->
   $("#save_link").click ->
     $("#memberz_form").submit()
--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -16,4 +16,14 @@ module MembersHelper
       "<td class=checkbox><input class='rck #{member.full_roles}' name='distributions[#{member.id}_email]' type='checkbox'></td>"
     end
   end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, h("add_fields(this, '#{association}', '#{escape_javascript(fields)}')"))
+#    link_to_function(name, h("add_fields(this, '#{association}', '#{fields}')"))
+  end
+
 end
