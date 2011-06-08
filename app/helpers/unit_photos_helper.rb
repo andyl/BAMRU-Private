9fa2151101904a122cf112117d3b05e737185dc0
andyl <andy@r210.com>
2011-06-08 11:57:06 -0700

    updated unit photo roster

--- app/helpers/unit_photos_helper.rb
+++ app/helpers/unit_photos_helper.rb
@@ -8,4 +8,13 @@ module UnitPhotosHelper
       "#{itag}<br/>#{name}"
     end
   end
+
+  def display_extra_photos(mem)
+    return "" if mem.nil? || mem.photos.length < 2
+    return "<tr align=center><td>-</td></tr>" if mem.nil? || mem.photos.length < 2
+    photo_links = mem.photos[1..-1].map do |p|
+      image_tag(p.image.url(:icon), :class => "roster_icon_photo")
+    end.join(' ')
+    "<tr height=30 align=center><td>#{photo_links}</td></tr>"
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,7 @@ class Photo < ActiveRecord::Base
   belongs_to :member
   acts_as_list :scope => :member_id
 
-  has_attached_file :image, :styles => {:medium => "300x300", :thumb => "100x100"}
+  has_attached_file :image, :styles => {:medium => "300x300", :roster => "150x150", :thumb => "100x100", :icon => "30x30"}
 
   # ----- Callbacks -----
 
