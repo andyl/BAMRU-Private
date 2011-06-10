71ea7aa77d98610ae5e30e12236f2179f9473c1a
andyl <andy@r210.com>
2011-06-10 14:32:00 -0700

    initial implementation editing avail_ops

--- app/controllers/do_assignments_controller.rb
+++ app/controllers/do_assignments_controller.rb
@@ -1,6 +1,6 @@
 class DoAssignmentsController < ApplicationController
   def index
-    @do_assignments = DoAssignment.order('year ASC')
+    @do_assignments = DoAssignment.order('year ASC').where(:quarter => Time.now.current_quarter).where(:year => Time.now.year)
   end
 
   def show
