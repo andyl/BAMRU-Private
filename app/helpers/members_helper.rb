7e6e3ac3a2ff87fc5f88da89352859a8d9aeb53b
andyl <andy@r210.com>
2011-06-22 16:51:15 -0700

    added info labels for "other info" and "emergency contacts"

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -21,8 +21,8 @@ module MembersHelper
     link_to_function(name, "remove_fields(this);")
   end
 
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.class.reflect_on_association(association).klass.new
+  def link_to_add_fields(name, f, association, params={})
+    new_object = f.object.class.reflect_on_association(association).klass.new(params)
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
