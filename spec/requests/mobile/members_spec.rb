c715b51483af137bb7699c61226a42e0779b559c
andyl <andy@r210.com>
2011-06-28 08:02:52 -0700

    added request spec for mobile login

--- spec/requests/mobile/members_spec.rb
+++ spec/requests/mobile/members_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
 describe "Members" do
-  describe "GET /members" do
+  describe "GET /mobile" do
     it "works!" do
-      get members_path
+      get mobile_path
       response.status.should be(302)
     end
     it "logs the user in" do
       user_name = "asdf_zxcv"
       Member.create!(:user_name => user_name)
       Member.count.should == 1
-      visit root_path
+      visit mobile_path
       Member.count.should == 1
-      current_path.should == login_path
+      current_path.should == mobile_login_path
       fill_in "user_name", :with => user_name
       fill_in "password",  :with => 'welcome'
       click_button 'Log in'
-      current_path.should == root_path
-      page.should have_content('welcome Asdf')
+      current_path.should == mobile_path
+      page.should have_content('BAMRU Mobile')
     end
   end
 end
