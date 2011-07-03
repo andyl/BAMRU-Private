ad2511174c81fb489670a8b26255a7d578fd9820
andyl <andy@r210.com>
2011-07-03 11:37:17 -0700

    simple chat is working (with glitches)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
   def broadcast(channel, &block)
-    message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
-    uri = URI.parse("http://localhost:9292/faye")
+    message = {:channel => channel, :data => capture(&block)}
+    uri = URI.parse("http://ekel:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
   
