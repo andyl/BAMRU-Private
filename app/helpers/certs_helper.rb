dfbde98c8d2deb8b04f17f5bb92181e83663790f
andyl <andy@r210.com>
2011-08-25 11:57:10 -0700

    reformatted certs#index page

--- app/helpers/certs_helper.rb
+++ app/helpers/certs_helper.rb
@@ -26,6 +26,7 @@ module CertsHelper
     return "" if cert.nil?
     return link_to("Doc Image", cert.cert.url) unless cert.cert_file.blank?
     return link_to("FCC", cert.link)           unless cert.link.blank?
+    return cert.comment                        unless cert.comment.blank?
     ""
   end
 
@@ -40,12 +41,29 @@ module CertsHelper
     mem == current_member || current_member.admin?
   end
 
+  def do_span(text, width)
+    "<span style='display: inline-block; width: #{width}px;'>#{text}</span>"
+  end
+
+  def add_link(mem, type)
+    link_to("add", new_member_cert_path(mem, :typ => type.to_s))
+  end
+
+  def cert_dump(mem, type)
+    certs = mem.certs.where(:typ => type)
+    certs.map do |cert|
+      exp = expiration(cert)
+      des = description(cert)
+      doc = documentation(cert)
+      act = can_update?(mem) ? td(cert_actions(mem, cert, type)) : ""
+      do_span(des, 100) + do_span(doc, 100) + do_span(exp, 100) + " " + act
+    end.join("<br/>")
+  end
+
   def cert_value(mem, type)
-    action = ()
     cert = mem.certs.where(:typ => type).first
     exp = td(expiration(cert))
     des = td(description(cert))
-    com = td(comment(cert))
     doc = td(documentation(cert))
     act = can_update?(mem) ? td(cert_actions(mem, cert, type)) : ""
     exp + des + doc + act
