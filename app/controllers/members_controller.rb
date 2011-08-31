be8ecf496c4d687170c3f3468cfe307e6d8a2e61
andyl <andy@r210.com>
2011-08-31 15:48:31 -0700

    revamped cache control

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,12 +6,13 @@ class MembersController < ApplicationController
   def index
     @client_ip = request.remote_ip
     @message = Message.new
-    if params['refresh'] == 'true'
-      expire_fragment("member_index_table-#{current_member.id}")
-      expire_fragment("unit_certs_table-#{current_member.id}")
-      expire_fragment("unit_avail_ops_table-#{current_member.id}")
-    end
-    unless fragment_exist?(:fragment => "member_index_table-#{current_member.id}")
+    @fragment_type = cookies['rsa_show'] == 'true' ? "all" : "active"
+    #if params['refresh'] == 'true'
+    #  expire_fragment("member_index_table-#{current_member.id}")
+    #  expire_fragment("unit_certs_table-#{current_member.id}")
+    #  expire_fragment("unit_avail_ops_table-#{current_member.id}")
+    #end
+    unless fragment_exist?(:fragment => "member_index_table-#{@fragment_type}")
       if cookies['rsa_show'] == 'true'
         @members = Member.order_by_role_score.all
       else
--- app/controllers/unit_certs_controller.rb
+++ app/controllers/unit_certs_controller.rb
@@ -3,12 +3,13 @@ class UnitCertsController < ApplicationController
   before_filter :authenticate_member!
 
   def index
-    if params['refresh'] == 'true'
-      expire_fragment("member_index_table-#{current_member.id}")
-      expire_fragment("unit_certs_table-#{current_member.id}")
-      expire_fragment("unit_avail_ops_table-#{current_member.id}")
-    end
-    unless fragment_exist?(:fragment => "unit_certs_table-#{current_member.id}")
+    @fragment_type = cookies['rsa_show'] == 'true' ? "all" : "active"
+    #if params['refresh'] == 'true'
+    #  expire_fragment("member_index_table-#{current_member.id}")
+    #  expire_fragment("unit_certs_table-#{current_member.id}")
+    #  expire_fragment("unit_avail_ops_table-#{current_member.id}")
+    #end
+    unless fragment_exist?(:fragment => "unit_certs_table-#{@fragment_type}")
       if cookies['rsa_show'] == 'true'
         @members = Member.order_by_role_score.all
       else
