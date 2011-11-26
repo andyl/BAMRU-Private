cb85fe5dd864420ecc32ecb4d73ad0b13999e017
andyl <andy@r210.com>
2011-11-26 09:31:51 -0800

    fixed mobile3 conditional contact display

--- app/assets/javascripts/mobile3/views/member_show.js.coffee
+++ app/assets/javascripts/mobile3/views/member_show.js.coffee
@@ -15,7 +15,7 @@ class @M3_MemberShowView extends Backbone.View
   divider: (label) ->
     "<div class='divider'>#{label}</div>"
   phone_helper: ->
-    return "" unless @model.hasPhone
+    return "" unless @model.hasPhone()
     display = _(@model.attributes.phones_attributes).map (phone) ->
       num  = phone.number
       typ  = phone.typ
@@ -28,15 +28,15 @@ class @M3_MemberShowView extends Backbone.View
       "<div class=navbox>#{val1}#{val2}</div>"
     @divider("Phones") + display.join('')
   email_helper: ->
-    return "" unless @model.hasEmail
+    return "" unless @model.hasEmail()
     display = _(@model.attributes.emails_attributes).map (email) ->
       adr  = email.address
       val1 = "<a class=nav href='mailto:#{adr}'>#{adr}</a>"
       "#{val1}"
     @divider("Emails") + display.join('')
   emergency_helper: ->
-    return "" unless @model.hasEmergencyContact
-    display = _(@model.attributes.emergency_contacts_attributes).map (emc) ->
+    return "" unless @model.hasContact()
+    display = _(@model.attributes.contacts_attributes).map (emc) ->
       nam = emc.name
       typ = emc.typ
       num = emc.number
--- app/decorators/member_decorator.rb
+++ app/decorators/member_decorator.rb
@@ -4,11 +4,11 @@ class MemberDecorator < ApplicationDecorator
   def mobile_json
     member_fields = %w(first_name last_name id typ)
     result = subset(model.attributes, member_fields)
-    result["full_roles"]                    = model.full_roles
-    result["phones_attributes"]             = phone_data     if has_phone?
-    result["emails_attributes"]             = email_data     if has_email?
-    result["emergency_contacts_attributes"] = emergency_data if has_emergency_contacts?
-    result["photos"]                        = photo_data     if has_photo?
+    result["full_roles"]          = model.full_roles
+    result["phones_attributes"]   = phone_data    if has_phone?
+    result["emails_attributes"]   = email_data    if has_email?
+    result["contacts_attributes"] = contacts_data if has_contacts?
+    #result["photos"]                        = photo_data     if has_photo?
     result.to_json
   end
 
@@ -21,13 +21,13 @@ class MemberDecorator < ApplicationDecorator
 
   private
 
-  def has_phone?() !model.phones.blank?; end
-  def has_email?() !model.emails.blank?; end
-  def has_photo?() !model.photos.blank?; end
-  def has_ham?()   !model.ham.blank?;    end
-  def has_v9?()    !model.v9.blank?;     end
-  def has_emergency_contacts?() !model.emergency_contacts.blank?; end
-  def has_other_infos?()        !model.other_infos.blank?;        end
+  def has_phone?()       !model.phones.blank?;             end
+  def has_email?()       !model.emails.blank?;             end
+  def has_photo?()       !model.photos.blank?;             end
+  def has_ham?()         !model.ham.blank?;                end
+  def has_v9?()          !model.v9.blank?;                 end
+  def has_contacts?()    !model.emergency_contacts.blank?; end
+  def has_other_infos?() !model.other_infos.blank?;        end
 
   def phone_data
     phone_fields = %w(number id typ)
@@ -39,7 +39,7 @@ class MemberDecorator < ApplicationDecorator
     model.emails.map {|email| subset(email.attributes, email_fields)}
   end
 
-  def emergency_data
+  def contacts_data
     emc_fields = %w(name number typ id)
     model.emergency_contacts.map {|emc| subset(emc.attributes, emc_fields)}
   end
