453041a87f24b6c0091a0fd53b4e7bedf7ae47b0
andyl <andy@r210.com>
2012-05-29 17:23:22 -0700

    updated postgres cap script

--- config/deploy/shared/packages/postgresql.rb
+++ config/deploy/shared/packages/postgresql.rb
@@ -3,7 +3,7 @@ namespace :postgresql do
 
   desc "Create a database user for this application."
   task :create_database_user, roles: :db, only: {primary: true} do
-    run %Q{#{sudo} -u postgres psql -c "create user #{application} with password '#{POSTGRES_PASS}';"}
+    run %Q{#{sudo} -u postgres psql -c "create user #{application} with password '#{POSTGRES_PASS}' createdb;"}
   end
-  after "deploy:setup", "postgresql:create_database_user"
+  before "deploy:setup", "postgresql:create_database_user"
 end
