34775b6381dd1fdfe945d9a6f4514fed89597258
andyl <andy@r210.com>
2012-06-05 18:55:34 -0700

    changed vagrant rails_env to staging

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -2,7 +2,7 @@ puts ' VAGRANT '.center(70, '-')
 
 set :proxy,     "vagrant"
 set :branch,    fetch(:branch, "dev")
-set :rails_env, fetch(:env,    "development")
+set :rails_env, fetch(:env,    "staging")
 
 server proxy, :app, :web, :db, :primary => true
 
