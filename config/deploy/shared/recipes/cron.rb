d2d1dedec3e760d5f12cff0088e960ac6756f2bf
andyl <andy@r210.com>
2012-06-07 14:11:17 -0700

    updated cron require

--- config/deploy/shared/recipes/cron.rb
+++ config/deploy/shared/recipes/cron.rb
@@ -3,12 +3,13 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   after 'deploy', 'cron:reset'
 
+  set(:whenever_environment) { stage }
+  require 'whenever/capistrano'
+
   namespace :cron do
 
     desc "Reset Cron"
     task :reset do
-      set(:whenever_environment) { stage }
-      require 'whenever/capistrano'
       run "cd #{current_path} && whenever --update-crontab #{application}"
     end
 
