147eb396384bbe84ef4f2f400a96ccd598d01af8
andyl <andy@r210.com>
2012-06-12 19:10:56 -0700

    update wiki integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,7 +26,7 @@ class HomeController < ApplicationController
     path = path.split('?').first unless path.blank?
     mw_file = "/home/aleak/.mw_auth/#{current_member.wiki_name}"
     ActiveSupport::Notifications.instrument("service.wiki", {:member => current_member})
-    system "ssh wiki.bamru.net 'touch #{mw_file}'"
+    system "ssh aleak@wiki.bamru.net 'touch #{mw_file}'"
     @link = "http://wiki.bamru.net#{path}?username=#{current_member.wiki_name}"
     #redirect_to @link
   end
