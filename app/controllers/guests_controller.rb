aad638de3392de1556a3d48be2cbe4a5a23a9b79
andyl <andy@r210.com>
2012-11-21 15:51:46 -0800

    added guest support

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -16,13 +16,44 @@ class GuestsController < ApplicationController
     @guest.password = ""
     @guest_name = @guest.full_name
   end
+
+  def new
+    @guest = Member.new
+    @guest.addresses.build
+    @guest.phones.build
+    @guest.emails.build
+  end
   
   def create
-    render :json => Member.create(params[:Member])
+    @guest = Member.create(params["member"])
+    if @guest.valid?
+      expire_fragment(/member_index_table/)
+      expire_fragment(/unit_certs_table/)
+      expire_fragment(/unit_avail_ops_table/)
+      expire_fragment('unit_photos_table')
+      redirect_to guest_path(@guest), :notice => "Successful Update"
+    else
+      render "new"
+    end
   end
   
   def update
-    respond_with Member.update(params[:id], params[:Member])
+    @guest = Member.where(:id => params[:id]).first
+    @guest_name = @guest.full_name
+    m_params = params["member"]
+    if m_params["password"].blank? && m_params["password_confirmation"].blank?
+      m_params.delete("password"); m_params.delete("password_confirmation")
+    end
+    x = @guest.update_attributes(m_params)
+    if x
+      expire_fragment(/member_index_table/)
+      expire_fragment(/unit_certs_table/)
+      expire_fragment(/unit_avail_ops_table/)
+      expire_fragment('unit_photos_table')
+      redirect_to guest_path(@guest), :notice => "Successful Update"
+    else
+      render "edit"
+    end
   end
   
   def destroy
