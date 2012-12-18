1cfaedc29468a5a3f0e5c8786e3f56a0709e307a
andyl <andy@r210.com>
2012-12-17 20:01:56 -0800

    update qc log messages

--- app/models/qc_calendar.rb
+++ app/models/qc_calendar.rb
@@ -4,7 +4,7 @@ class QcCalendar
 
   def self.csv_resync
     result = open('http://bamru.org/csv_resync')
-    puts result
+    puts "Doing CSV Resync - Status = #{result.string}"
   end
 
 end
--- app/models/qc_gcal.rb
+++ app/models/qc_gcal.rb
@@ -78,7 +78,7 @@ class QcGcal
     return puts("can't create operation") if ar_event.typ == "operation"
     gcal_service = authenticate_and_return_gcal_service
     gcal_event   = Event.new(gcal_service)
-    puts "Creating #{ar_event.id}"
+    puts "Creating gCal Event id:#{ar_event.id}"
     save_event_to_gcal(gcal_service, gcal_event, ar_event)
   end
 
@@ -95,7 +95,7 @@ class QcGcal
   def self.delete_event(id)
     gcal_service = authenticate_and_return_gcal_service
     gcal_events  = Event.find(gcal_service, "BE#{id}")
-    gcal_events.each {|ev| puts "Deleting #{id}" ; ev.delete }
+    gcal_events.each {|ev| puts "Creating gCal Event id:#{id}" ; ev.delete }
   end
 
 end
