c7dd811870621407d77de0fe234020b16c2e4540
andyl <andy@r210.com>
2013-01-18 11:56:58 -0800

    finished meeting AARs

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Zn
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
+    # see https://github.com/ryandotsmith/queue_classic
+    config.active_record.schema_format = :sql
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
