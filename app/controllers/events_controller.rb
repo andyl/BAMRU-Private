d88acc1dd159a8454debb47b2c39b338f32c2c4c
andyl <andy@r210.com>
2013-01-18 20:20:54 -0800

    added last-modified caching to events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,18 @@
 class EventsController < ApplicationController
 
-  before_filter :authenticate_member!
+  #before_filter :authenticate_member!
+  before_filter :authenticate_member_with_basic_auth!
+
+  #curl -u user_name:pass http://bamru.net/reports
 
   def index
     @session_id  = session["session_id"]
+    @members     = Member.scoped
+    @events      = Event.scoped
     @member_json = current_member.to_json only: [:id, :first_name, :last_name, :typ, :admin, :developer]
+    max_mem = @members.maximum(:updated_at)
+    max_evt = @events.maximum(:updated_at)
+    fresh_when last_modified: [max_mem, max_evt].max
   end
 
 end
