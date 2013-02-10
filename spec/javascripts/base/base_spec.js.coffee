41b28217c7860cb70bbf8c63c71d811ad703a471
andyl <andy@r210.com>
2013-02-09 16:11:12 -0800

    jasminerice base spec is working

---
+++ spec/javascripts/base/base_spec.js.coffee
@@ -0,0 +1,4 @@
+describe "BaseSpec", ->
+
+  describe "some scenario", ->
+    it "works", -> expect(1).toEqual(1)
--- spec/javascripts/spec.js
+++ spec/javascripts/spec.js
@@ -1,14 +1,3 @@
-//= require zepto-min
-
 //= require jquery
-//= require underscore-min-1_2_1
-//= require backbone-min-0_5_3
 
-//= require jasmine-jquery-1_3_1
-//= require sinon-1_2_0
-
-//= require mobile/application
-
-//= require_tree ./chat2
-//= require_tree ./mobile1
-//= require_tree ./mobile
+//= require_tree ./base
