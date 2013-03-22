9a86f9136d010c2a1ff412d3cc8affa987732782
andyl <andy@r210.com>
2013-03-22 09:45:11 -0700

    more tweaks

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,13 +11,18 @@ Zn::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.assets.compress = true
+  config.assets.compile  = false
+  config.assets.digest   = true
+  config.assets.initialize_on_precompile = false
+
   # Compress both stylesheets and JavaScripts
-  config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
+  #config.assets.js_compressor  = :uglifier
+  #config.assets.css_compressor = :scss
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
+  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # Use 'X-Accel-Redirect' for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
