b8e53b81ae36164ac5de2d65bc6ba425272986ec
andyl <andy@r210.com>
2013-03-24 21:00:31 -0700

    dev test

--- config/deploy/shared/packages/foreman.rb
+++ config/deploy/shared/packages/foreman.rb
@@ -2,14 +2,20 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   after 'deploy:update', 'foreman:export'
   after 'deploy:update', 'foreman:restart'
-  after 'deploy:rollback', 'foreman:export'
-  after 'deploy:rollback', 'foreman:restart'
+  after 'deploy:rollback:revision', 'foreman:export_rollback'
+  #after 'deploy:rollback', 'foreman:restart'
 
   namespace :foreman do
     desc "Export the Procfile to Ubuntu's upstart scripts"
     task :export, :roles => :app do
       run "#{sudo} rm -f /etc/init/#{app_name}*.conf"
       run "cd #{release_path} && foreman export upstart /tmp/xinit -e .rbenv-vars -p #{web_port} -a #{app_name} -u #{user} -l #{shared_path}/log"
+      run "#{sudo} mv /tmp/xinit/* /etc/init"
+      run "rm -rf /tmp/xinit"
+    end
+    task :export_rollback, :roles => :app do
+      run "#{sudo} rm -f /etc/init/#{app_name}*.conf"
+      run "cd #{current_path} && foreman export upstart /tmp/xinit -e .rbenv-vars -p #{web_port} -a #{app_name} -u #{user} -l #{shared_path}/log"
       run "#{sudo} mv /tmp/xinit/* /etc/init"
       run "rm -rf /tmp/xinit"
     end
