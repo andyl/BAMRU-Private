f9c2d511e398086799942ecdc3a1365b572225f1
andyl <andy@r210.com>
2013-11-16 11:07:15 -0800

    updated events

--- lib/export/events.rb
+++ lib/export/events.rb
@@ -3,10 +3,30 @@ require 'csv'
 module Export
   class Events
     def self.all
+      per_arr = []
+      par_arr = []
       ev_arr = Event.all.map do |eve|
-        parts = eve.periods.map do |per|
+        eve.periods.each do |per|
+          per.participants.each do |par|
+            par_arr << [
+              par.member.try(:emails).try(:first).try(:address),
+              par.period_id,
+              par.en_route_at,
+              par.return_home_at,
+              par.signed_in_at,
+              par.signed_out_at,
+              par.ol,
+              par.ahc
+            ]
+          end
+          per_arr << [
+            per.id,
+            per.event_id,
+            per.position
+          ]
         end
         [
+          eve.id,
           eve.typ,
           eve.title,
           eve.leaders,
@@ -23,6 +43,16 @@ module Export
       CSV.open('/tmp/events.csv', 'w') do |csv|
         ev_arr.each do |mem|
           csv << mem
+        end
+      end
+      CSV.open('/tmp/periods.csv', 'w') do |csv|
+        per_arr.each do |per|
+          csv << per
+        end
+      end
+      CSV.open('/tmp/participants.csv', 'w') do |csv|
+        par_arr.each do |par|
+          csv << par
         end
       end
     end
