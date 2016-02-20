133bdfe9a5dd87aae60593cbc2ae973c55c1c9a6
andyl <andy@r210.com>
2016-02-20 13:55:48 -0800

    Tweak cron script

--- script/cron_backup.sh
+++ script/cron_backup.sh
@@ -5,15 +5,13 @@
 # to just grab the database:
 # cap backup:download:db
 #
-cd /home/aleak/lr/BAMRU-Private
-source .env
 echo "||||||||||||||||||||||||||||"
 echo ------- start backup -------
 date
+cd /home/aleak/lr/BAMRU-Private
 echo PWD is `pwd`
-echo APP_NAME is $APP_NAME
 echo ----------------------------
-bundle exec cap backup:download:all
+source .env ; bundle exec cap backup:download:all
 echo ------- finish backup ------
 date
 echo ----------------------------
